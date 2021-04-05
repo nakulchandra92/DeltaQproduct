b95425e61885b80480f1a98f5562305d5f93e640
nakulchandra92 <77980351+nakulchandra92@users.noreply.github.com>
2021-04-06 01:17:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 OUTPUT=$(dig +short myip.opendns.com @resolver1.opendns.com)
-sed -i "s/machineip/$OUTPUT/" frontend/src/app/csvupload/csvupload.service.ts
+sed -i "s/localhost/$OUTPUT/" frontend/src/app/csvupload/csvupload.service.ts
+
